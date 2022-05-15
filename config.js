a9f440af23f5c5c129cbcd35b7bb36e21179985d
ManpreeSingh <95350396+ManpreeSingh@users.noreply.github.com>
2022-05-15 17:37:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
    /* EDIT THIS*/
 // Your name, real or Nick
-const name = "Nice";
+const name = "Manpreet";
 // What you are? Cool? Legend? Best? or anything else
-const guyType = "Cool";
+const guyType = "Fantastic";
 // Entera contact email. Leave empty If you don't want to show a Contact Us button.
 const contact_email = "";
 /* CONFIGURATION IS AS SIMPLE AS THAT!*/
